71c3746354271a19c47c4f8eed28e57c64587181
FoxPlays <foxplaysmail@mail.ru>
2024-03-31 20:04:39 +0700

    BlockBar.jsx: added "..." (more) button

--- client/src/components/BlockBar.jsx
+++ client/src/components/BlockBar.jsx
@@ -9,7 +9,7 @@ const BlockBar = () => {
 
   return (
   <div className="bg-white shadow-xl ml-12 hidden lg:block h-screen overflow-y-scroll scrollbar-hide">
-    <div className="fixed bg-white text-2xl truncate text-center select-none pb-6 rounded-2xl z-10">
+    <div className="fixed bg-white text-2xl truncate text-center select-none pb-6 rounded-b-2xl z-10">
       <div className="flex flex-row gap-8 mx-8 my-4 justify-center">
         <div className="w-16 h-14 bg-[#DDDFE1] rounded shadow-inner" />
         <div className="w-16 h-14 bg-[#DDDFE1] rounded shadow-inner" />
@@ -22,6 +22,9 @@ const BlockBar = () => {
       {items.map((item) => (
         <BlockBarComponent image={item.image} />
       ))}
+      <a href='.' className="pt-2 pb-10">
+        <span className="text-2xl bg-[#DDDFE1] px-8 py-2 rounded-2xl align-top">...</span>
+      </a>
     </div>
   </div>
   );
