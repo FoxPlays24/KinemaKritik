8cc1564f0e700ca6d85351b05783e18ea111001b
FoxPlays <foxplaysmail@mail.ru>
2024-03-31 17:23:47 +0700

    BlockBar.jsx: added "scrollbar-hide"

--- client/src/components/BlockBar.jsx
+++ client/src/components/BlockBar.jsx
@@ -8,7 +8,7 @@ const BlockBar = () => {
   ];
 
   return (
-  <div className="bg-white shadow-xl ml-12 hidden lg:block h-screen overflow-y-scroll">
+  <div className="bg-white shadow-xl ml-12 hidden lg:block h-screen overflow-y-scroll scrollbar-hide">
     <div className="fixed bg-white text-2xl truncate text-center select-none pb-6 rounded-2xl z-10">
       <div className="flex flex-row gap-8 mx-8 my-4 justify-center">
         <div className="w-16 h-14 bg-[#DDDFE1] rounded shadow-inner" />
