b7537e3d16ed6d60766500bd07e148116302cc21
FoxPlays <foxplaysmail@mail.ru>
2024-04-05 00:06:42 +0700

    index.js: added toasters

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { Toaster } from 'react-hot-toast'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
+    <Toaster />
     <App />
   </React.StrictMode>
 );
