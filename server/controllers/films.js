b42a39e4ca903cfd9cccb7cc18fef9b7de88e3e4
FoxPlays <foxplaysmail@mail.ru>
2024-04-05 00:10:38 +0700

    films.js: added more columns to request  in getFilms and getFilm api calls

--- server/controllers/films.js
+++ server/controllers/films.js
@@ -1,7 +1,8 @@
 import { db } from "../db.js"
 
 export const getFilms = (req, res) => {
-    db.query(`SELECT films.id, title, original_title, description, film_types.name film_type, age_limits.name age_limit FROM films JOIN film_types ON film_types.id=film_type JOIN age_limits ON age_limits.id=age_limit GROUP BY films.id`, (err,data) => {
+    db.query(`SELECT films.id, title, original_title, description, film_types.name film_type, age_limits.name age_limit, release_date, release_date_streams, release_date_russia
+              FROM films JOIN film_types ON film_types.id=film_type JOIN age_limits ON age_limits.id=age_limit GROUP BY films.id`, (err,data) => {
         if(err) return res.json(err)
         res.json(data)
     })
@@ -9,7 +10,8 @@ export const getFilms = (req, res) => {
 
 export const getFilm = (req, res) => {
     let id = req.params.film_id
-    db.query(`SELECT films.id, title, original_title, description, film_types.name film_type, age_limits.name age_limit FROM films JOIN film_types ON film_types.id=film_type JOIN age_limits ON age_limits.id=age_limit WHERE films.id=?`, id, (err,data) => {
+    db.query(`SELECT films.id, title, original_title, description, film_types.name film_type, age_limits.name age_limit, release_date, release_date_streams, release_date_russia
+              FROM films JOIN film_types ON film_types.id=film_type JOIN age_limits ON age_limits.id=age_limit WHERE films.id=?`, id, (err,data) => {
         if(err) return res.json(err)
         res.json(data)
     })
