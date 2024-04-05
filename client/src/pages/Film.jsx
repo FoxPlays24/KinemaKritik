35f75d5bee65de1b81279d3db730e28a7185075b
FoxPlays <foxplaysmail@mail.ru>
2024-04-06 04:15:12 +0700

    Film.jsx: added more information to display about films - primary color, genres, release dates and other

--- client/src/pages/Film.jsx
+++ client/src/pages/Film.jsx
@@ -10,12 +10,15 @@ const Film = () => {
   const [film,setFilm] = useState([])
   const [banner,setBanner] = useState([])
   const [trailer,setTrailer] = useState([])
+  const [primaryColor, setPrimaryColor] = useState([])
+  const [genres,setGenres] = useState([])
   
   useEffect(() => {
     const fetchFilm = async () => {
       try {
         const res = await axios.get(`http://localhost:80/films/${filmId}`)
         setFilm(res.data[0])
+        setPrimaryColor(res.data[0].page_primary_color.length ? res.data[0].page_primary_color : '#000')
       } catch (err) {
         console.error(err)
       }
@@ -41,6 +44,16 @@ const Film = () => {
       }
     }
     fetchTrailer()
+
+    const fetchGenres = async () => {
+      try {
+        const res = await axios.get(`http://localhost:80/films/${filmId}/genres`)
+        setGenres(res.data)
+      } catch (err) {
+        console.error(err)
+      }
+    }
+    fetchGenres()
   }, [filmId])
 
   return (
@@ -53,14 +66,14 @@ const Film = () => {
         <div className='flex flex-row items-center'>
           <div className='flex gap-2 font-roboto items-center'>
             <span className='text-2xl font-medium'>{film.title}</span>
-            <span className='text-sm text-[#C64137]'>+240</span>
+            <span className={`text-[${primaryColor}] text-sm`}>+240</span>
           </div>
           <span className='text-sm font-roboto text-end ml-auto'>{film.age_limit} {film.original_title} {new Date(film.release_date).getFullYear()} Кристофер Нолан</span>
         </div>
 
         <div className='flex flex-row items-center'>
           <div className='flex flex-row gap-4 select-none'>
-            <a href='.' className='text-sm text-white bg-[#C64137] rounded-2xl px-11 py-2 cursor-default'>
+            <a href='.' className={`text-sm text-white bg-[${primaryColor}] rounded-2xl px-11 py-2 cursor-default`}>
               Инфо
             </a>
             <a href='.' className='text-sm bg-white rounded-2xl px-11 py-2 border-black border transition hover:bg-zinc-200'>
@@ -79,25 +92,60 @@ const Film = () => {
           </div>
         </div>
       </div>
-      <div className='flex flex-row items-center mt-8'>
-        <div className='flex flex-col bg-red-200'>
+      <div className='flex flex-row items-center mt-8 select-none gap-8'>
+        <div className='flex flex-col w-[100%] relative'>
+          {/* Dates */}
           <div>
             <span>Дата выхода</span>
-            <span className='font-medium ml-auto'>2023 июль 19</span>
+            <span className='font-medium absolute right-0'>{new Date(film.release_date).toLocaleString("ru", { year: 'numeric', month: 'long', day:'numeric' })}</span>
           </div>
-          <div className='text-zinc-400'>
-            <span>{'>'} стриминг-сервисы</span>
-            <span className='ml-auto'>2023 ноябрь 21</span>
+          {
+            film.release_date_streams &&
+            <div className='text-zinc-400'>
+              <span>{'>'} стриминг-сервисы</span>
+              <span className='absolute right-0'>{new Date(film.release_date_streams).toLocaleString("ru", { year: 'numeric', month: 'long', day:'numeric' })}</span>
+            </div>
+          }
+          {
+            film.release_date_russia &&
+            <div className='text-zinc-400'>
+              <span>{'>'} в России</span>
+              <span className='absolute right-0'>{new Date(film.release_date_russia).toLocaleString("ru", { year: 'numeric', month: 'long', day:'numeric' })}</span>
+            </div>
+          }
+          {/* Genres */}
+          <div>
+            <span>Жанр</span>
+            <span className='absolute right-0'>
+              {genres.map((genre, index) => (
+                <span>
+                  <a href={`/genre/${genre.id}`} className='font-medium'>{genre.name}</a>
+                  {index !== genres.length-1 && ' / '}
+                </span>
+              ))}
+            </span>
           </div>
-          <div className='text-zinc-400'>
-            <span>{'>'} в России</span>
-            <span className='ml-12'>2023 сентябрь 21</span>
+          {/* Country */}
+          <div>
+            <span>Страна</span>
+            <span className='font-medium absolute right-0'>США / Великобритания</span>
+          </div>
+          {/* Rating */}
+          <div>
+            <span>Рейтинг</span>
+            <span className='font-medium absolute right-0'>{film.age_limit}</span>
           </div>
         </div>
-        <a href='https://youtu.be/bK6ldnjE3Y0' className='ml-auto bg-red-100'>
-          <div className='fixed flex items-center gap-2 text-white font-semibold text-2xl z-10'>{<FaPlay />} Трейлер</div>
-          <img src={trailer} alt='Trailer' className='rounded-2xl brightness-50 transition hover:brightness-75 w-72' />
-        </a>
+        {/* Trailer */}
+        {
+          trailer &&
+          <a href='https://youtu.be/bK6ldnjE3Y0' className='relative'>
+            <div className='absolute z-10 translate-x-1/2 translate-y-14'>
+              <span className='flex items-center gap-2 text-white font-semibold text-2xl'>{<FaPlay />} Трейлер</span>
+            </div>
+            <img src={trailer} alt='Trailer' className='rounded-2xl brightness-50 transition hover:brightness-75 w-[460px]' />
+          </a>
+        }
       </div>
     </>
   )
