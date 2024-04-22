f692bc3932497da7999e55c021f1858396bd651c
FoxPlays <foxplaysmail@mail.ru>
2024-04-23 02:29:40 +0700

    Use 'dotenv' for database connection data

--- server/db.js
+++ server/db.js
@@ -1,10 +1,11 @@
 import mysql from 'mysql2'
+import 'dotenv/config'
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '0000oooo',
-    database: 'kinemakritik'
+    host:     process.env.DB_HOST,
+    user:     process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
 }).promise()
 
 export default db
